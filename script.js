da594161da8db4e32194870918a4f53e647af755
Dragoş <edragos33@gmail.com>
2025-05-08 16:01:17 +0200

    created popup

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
+import * as THREE from 'three';
+
 const sideMenu = document.querySelector(".sidebar");
 const menuBtn = document.querySelector(".menu-btn");
+const content = document.querySelector(".content");
+// -- Planets buttons --
+const sunBtn = document.querySelector(".sunBtn");
 
 menuBtn.addEventListener("click", function(){
     if (sideMenu.style.width === "250px"){
         sideMenu.style.width = "70px";
         menuBtn.innerText = "Open";
+        content.classList = "hiden";
     } else{
         sideMenu.style.width = "250px";
         menuBtn.innerText = "Close";
+        setTimeout(() => {
+            content.classList = "content";
+        }, "300");
     }
 });
 
@@ -16,4 +25,22 @@ document.addEventListener("click", function(e){
         sideMenu.style.width = "70px";
         menuBtn.innerText = "Open";
     }
-})
+});
+
+
+function showPlanet({name, texturePath, radius}){
+    
+}
+
+sunBtn.addEventListener("click", function(){
+    const infoPage = document.createElement("div");
+    infoPage.classList = "infoPage";
+
+    let infoTitle = document.createElement("h2");
+    infoTitle.innerText = "The Sun";
+
+    infoPage.appendChild(infoTitle);
+
+    document.body.appendChild(infoPage);
+});
+
